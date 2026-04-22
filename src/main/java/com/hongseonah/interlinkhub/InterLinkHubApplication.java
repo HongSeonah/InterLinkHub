6637c26bedaf69f2294104c5be0e501d8b53876f
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2026-04-22 16:10:11 +0900

    초기 세팅

---
+++ src/main/java/com/hongseonah/interlinkhub/InterLinkHubApplication.java
@@ -0,0 +1,13 @@
+package com.hongseonah.interlinkhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterLinkHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterLinkHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hongseonah/interlinkhub/InterLinkHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongseonah.interlinkhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterLinkHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
