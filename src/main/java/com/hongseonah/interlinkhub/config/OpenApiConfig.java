b36243cb1cdb5b2d6f0f30ab23e9ef277c8b18e1
홍선아 <144754093+HongSeonAh@users.noreply.github.com>
2026-04-22 17:27:18 +0900

    chore: jackson 설정, swagger 구현

---
+++ src/main/java/com/hongseonah/interlinkhub/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.hongseonah.interlinkhub.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Interlink Hub API")
+                        .description("보험사 금융 IT 인터페이스 통합관리시스템 API 문서")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Swagger UI")
+                        .url("/swagger-ui/index.html"));
+    }
+}
